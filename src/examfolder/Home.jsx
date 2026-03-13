a07f7ba51acfa8cd3ecb2e606c13bfaf0459dfee
meghana <2500032486@kluniversity.in>
2026-03-13 11:41:38 +0530

    modified project v2.0

--- src/examfolder/Home.jsx
+++ src/examfolder/Home.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h2>Employee Management System</h2>
       <p>Welcome to the Employee Management System.</p>
+
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+        alt="Employee Management"
+        style={{ width: "300px", marginTop: "20px" }}
+      />
     </div>
   );
 }
